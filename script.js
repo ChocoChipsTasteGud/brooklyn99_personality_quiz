d065832179db9ecfb4ab914ba60737048f965252
Aafia Rasheed <aafiarasheed313@gmail.com>
2026-06-11 21:37:16 +0400

    connecting JS to html + coding for changing pages + writing up quiz qs

---
+++ script.js
@@ -0,0 +1,97 @@
+let startClick = document.querySelector('.start-button');
+startClick.addEventListener('click', () => {
+    window.location.href = 'questions.html'
+})
+
+
+const question = document.querySelector(".question");
+const label = document.querySelectorAll("label");
+const nextButton = document.querySelector('.next');
+const radioButton = document.querySelectorAll('input[type=radio');
+
+let questionCount = 1;
+
+nextButton.addEventListener('click', () => {
+
+    qsCount = qsCount + 1;
+    if (qsCount === 2) {
+        question.textContent = "2) What does your usual weekend plan look like?";
+        label[0].textContent = "date";
+        label[1].textContent = "work";
+        label[2].textContent = "family time";
+        label[3].textContent = "random side quests";
+    }
+
+    if (qsCount === 3) {
+        question.textContent = "3) Your friend just texted you an achievement. What's most likely to be your response?";
+        label[0].textContent = "omg that's CRAZY reminds me of the time.. blahblah";
+        label[1].textContent = "👍";
+        label[2].textContent = "LET'S GO CONGRATSS";
+        label[3].textContent = "*leaves on read*";
+    }
+
+    if (qsCount === 4) {
+        question.textContent = "4) How would your friends describe you?";
+        label[0].textContent = "gym rat";
+        label[1].textContent = "baddie";
+        label[2].textContent = "weirdo";
+        label[3].textContent = "prankster";
+    }
+
+    if (qsCount === 5) {
+        question.textContent = "5) Who are you listening to?";
+        label[0].textContent = "Taylor Swift";
+        label[1].textContent = "Beethoven";
+        label[2].textContent = "Beyoncé";
+        label[3].textContent = "Eddie Money";
+    }
+
+    if (qsCount === 6) {
+        question.textContent = "6) What's your usual attitude in contests?";
+        label[0].textContent = "competitive";
+        label[1].textContent = "don't care";
+        label[2].textContent = "here for moral support";
+        label[3].textContent = "secretly competitive";
+    }
+
+    if (qsCount === 7) {
+        question.textContent = "Why is work important to you?";
+        label[0].textContent = "to serve others";
+        label[1].textContent = "it's fun";
+        label[2].textContent = "money";
+        label[3].textContent = "work friends";
+    }
+
+    if (qsCount === 8) {
+        question.textContent = "8) How social do you see yourself?";
+        label[0].textContent = "option1";
+        label[1].textContent = "option2";
+        label[2].textContent = "option3";
+        label[3].textContent = "option4";
+    }
+
+    if (qsCount === 9) {
+        question.textContent = "9) How grounded are you?";
+        label[0].textContent = "option1";
+        label[1].textContent = "option2";
+        label[2].textContent = "option3";
+        label[3].textContent = "option4";
+    }
+
+    if (qsCount === 10) {
+        question.textContent = "10) How likely are you to undertake a bet?";
+        label[0].textContent = "option1";
+        label[1].textContent = "option2";
+        label[2].textContent = "option3";
+        label[3].textContent = "option4";
+    }
+
+
+    
+    if (qsCount === 11) {
+        window.location.href = "results";
+    }
+});
+
+
+
